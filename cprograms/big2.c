49175f78fff408804fa0a264ef2ec15241e194bb
Cloud User <ec2-user@ip-172-31-9-134.us-west-1.compute.internal>
2022-11-08 12:05:28 +0000

    c program files copied for testing

---
+++ cprograms/big2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int A, B;
+ 
+    printf("Enter the numbers A, B: ");
+    scanf("%d %d %d", &A, &B);
+ 
+    if (A >= B)
+        printf("%d is the largest number.", A);
+ 
+    if (B >= A)
+        printf("%d is the largest number.", B);
+ 
+    return 0;
+}
